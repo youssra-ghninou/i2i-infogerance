0b07f74fbda2884bb8f0d1d43f22bb39269acc68
youssra-ghninou <ghninou177u@gmail.com>
2022-07-25 15:56:11 +0100

    header desktop done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,42 @@ import { AiOutlineMenuFold } from 'react-icons/ai';
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between">
+        <nav className="flex justify-between lg:items-center">
             <div className="w-1/12">
                 <img src={Logo} alt="logo" />
             </div>
-            <div className="navigation hidden md:block">
-                navig
+            <div className="navigation hidden lg:flex lg:gap-5 lg:text-gray lg:text-[16px] nd:text-bold lg:font-semibold">
+                <a href="" className="home">
+                    <div className="home lg:text-black ">
+                        Home
+                    </div>
+                </a>
+                <a href="" className="home">
+                    <div className="home">
+                        Who are We ?
+                    </div>
+                </a>
+                <a href="" className="home">
+                    <div className="home">
+                        Our Services
+                    </div>
+                </a>
+                <a href="" className="home">
+                    <div className="home">
+                        Our Projects
+                    </div>
+                </a>
+                <a href="" className="home">
+                    <div className="home">
+                        Contact Us
+                    </div>
+                </a>
+
             </div>
-            <div className="quote hidden md:block">
-                quote
-            </div>
-            <button className="menu md:hidden">
+                <button className='lg:bg-primary lg:p-4 lg:text-white lg:rounded-md lg:font-semibold text-[12px]'>
+                    Get a quote
+                </button>
+            <button className="menu lg:hidden">
                  <AiOutlineMenuFold />
             </button>
         </nav>
