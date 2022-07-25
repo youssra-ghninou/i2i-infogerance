031b9c27074840b39ed2a3c8076592bbc0f6dec3
aimade <draimade@gmail.com>
2022-07-25 17:59:57 +0100

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,23 +34,13 @@ const Navbar = () => {
                         Contact Us
                     </div>
                 </a>
-<<<<<<< HEAD
-            </div>
-            <div className="hidden md:block md:bg-primary md:p-4 text-white font-bold md:rounded-lg">
-                Get a Quote
-            </div>
-            <button className="menu md:hidden">
-            <IconContext.Provider value={{className: "text-2xl" }}>
-=======
 
             </div>
-                <button className='lg:bg-primary lg:p-4 lg:text-white lg:rounded-md lg:font-semibold text-[12px]'>
+                <button className='hidden lg:flex lg:bg-primary lg:p-4 lg:text-white lg:rounded-md lg:font-semibold text-[12px]'>
                     Get a quote
                 </button>
             <button className="menu lg:hidden">
->>>>>>> 0b07f74fbda2884bb8f0d1d43f22bb39269acc68
                  <AiOutlineMenuFold />
-            </IconContext.Provider>
             </button>
         </nav>
     );
